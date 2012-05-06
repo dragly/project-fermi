ee59d92da77e79ab4358f8d55210bd78327ccdf5
unknown <jorgen.hogberget@fys.uio.no>
2012-05-06 13:42:36 +0200

    Check platform.h for comments.

--- src/platform/platform.h
+++ src/platform/platform.h
@@ -16,6 +16,20 @@ public:
     virtual Sprite *createSprite(std::string spriteFile) = 0;
     virtual void drawSprite(Sprite* sprite, double x, double y, double width, double height, double rotation) = 0;
     virtual void clear() = 0;
+
+	void set_pixelres(int pixelres) {
+		this->pixelres = pixelres;
+	}
+
+protected:
+
+	//Is this a reasonable way to do things? I want to keep it out of the engine-class, since they are totally disconnected.
+	//Doing the appropritate conversions from meter to pixels just seems more logical in the graphics class...
+	//Converting angels etc. should also be done outside the engine... Are they?
+
+	//Very nice job, you have to give me a brief intro to the code sometime; atm I'm afraid to touch anything! :P
+	int pixelres;
+
 };
 
 #endif // PLATFORM_H
