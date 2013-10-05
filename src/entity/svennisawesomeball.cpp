7b5c82b0138ae60fbd3c0b85b85aef456514a7b7
jorgehog <jorgen_da_mad@hotmail.com>
2013-10-05 20:54:45 +0200

    made the example a bit more handsome.

--- src/entity/svennisawesomeball.cpp
+++ src/entity/svennisawesomeball.cpp
@@ -1,7 +1,7 @@
 #include "svennisawesomeball.h"
 
 SvennisAwesomeBall::SvennisAwesomeBall(GameEngine *engine, const b2Vec2 &position, double radius) :
-    circleEntity(engine, position, radius, "ball.png", true)
+    circleEntity(engine, position, radius, "svenniball.png", true)
 {
     createFixtureFromProps(1.0, 0.4, 0.3);
 }
